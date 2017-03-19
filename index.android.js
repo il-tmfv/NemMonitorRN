84c868f7a8dcffda814b3db9317c249e6f0ec0b4
Ilya Timofeev <il.tmfv@gmail.com>
2017-03-19 22:50:40 +0300

    Add fake data fetching

--- index.android.js
+++ index.android.js
@@ -10,25 +10,47 @@ import {
   StyleSheet,
   Text,
   Button,
-  View
+  ActivityIndicator,
+  View,
+  ToastAndroid,
 } from 'react-native';
 
 import Price from 'app/components/Price';
 
 export default class NemMonitorRN extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false, price: 100.00 }
+  }
+
+  onUpdate = () => {
+    ToastAndroid.show('Starting to fetch data...', ToastAndroid.SHORT);
+    this.setState({ loading: true, price: this.state.price + 1 });
+    setTimeout(() => {
+      console.log('unclick');
+      this.setState({ loading: false });
+      ToastAndroid.show('Success!', ToastAndroid.SHORT);
+    }, 5000);
+  }
+
   render() {
+    const { loading, price } = this.state;
+
     return (
       <View style={styles.container}>
         <View style={styles.currencies}>
-          <Price currency="BTC" amount={123.00}/>
+          <Price currency="BTC" amount={price}/>
           <Price currency="USD" amount={321.01}/>
         </View>
         <View style={styles.buttons}>
           <Button
+            disabled={loading}
+            onPress={this.onUpdate}
             title="Update"
             color="green"
             accessibilityLabel="Reload data from server"
           />
+          <ActivityIndicator animating={loading} size={60}/>
         </View>
       </View>
     );
